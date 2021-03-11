8894f1f54cdf25a87590749abbb289df3a49840a
kylec43 <kylecurry43@gmail.com>
2021-03-10 21:47:51 -0600

    commit

--- lib/screen/detailedview_screen.dart
+++ lib/screen/detailedview_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson3/model/constant.dart';
 import 'package:lesson3/model/photomemo.dart';
+import 'package:lesson3/screen/myview/myimage.dart';
 
 class DetailedViewScreen extends StatefulWidget {
   static const routeName = '/detailedViewScreen';
@@ -15,6 +16,8 @@ class _DetailedViewState extends State<DetailedViewScreen> {
   _Controller con;
   User user;
   PhotoMemo onePhotoMemo;
+  bool editMode = false;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -33,9 +36,76 @@ class _DetailedViewState extends State<DetailedViewScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Detailed View'),
+        actions: [
+          editMode
+              ? IconButton(icon: Icon(Icons.check), onPressed: con.update)
+              : IconButton(icon: Icon((Icons.edit)), onPressed: con.edit),
+        ],
       ),
-      body: Text(
-        onePhotoMemo.imageLabels.join(' | '),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: MyImage.network(
+                  url: onePhotoMemo.photoURL,
+                  context: context,
+                ),
+              ),
+              TextFormField(
+                enabled: editMode,
+                style: Theme.of(context).textTheme.headline6,
+                decoration: InputDecoration(
+                  hintText: 'Enter title',
+                ),
+                initialValue: onePhotoMemo.title,
+                autocorrect: true,
+                validator: PhotoMemo.validateTitle,
+                onSaved: null,
+              ),
+              TextFormField(
+                enabled: editMode,
+                decoration: InputDecoration(
+                  hintText: 'Enter memo',
+                ),
+                initialValue: onePhotoMemo.memo,
+                autocorrect: true,
+                keyboardType: TextInputType.multiline,
+                maxLines: 6,
+                validator: PhotoMemo.validateMemo,
+                onSaved: null,
+              ),
+              TextFormField(
+                enabled: editMode,
+                decoration: InputDecoration(
+                  hintText: 'Enter Shared With (email list)',
+                ),
+                initialValue: onePhotoMemo.sharedWith.join(','),
+                autocorrect: false,
+                keyboardType: TextInputType.multiline,
+                maxLines: 2,
+                validator: PhotoMemo.validateSharedWith,
+                onSaved: null,
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Constant.DEV
+                  ? Text('Image labels generate by ML',
+                      style: Theme.of(context).textTheme.bodyText1)
+                  : SizedBox(
+                      height: 1.0,
+                    ),
+              Constant.DEV
+                  ? Text(onePhotoMemo.imageLabels.join(' | '))
+                  : SizedBox(
+                      height: 1.0,
+                    ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -44,4 +114,12 @@ class _DetailedViewState extends State<DetailedViewScreen> {
 class _Controller {
   _DetailedViewState state;
   _Controller(this.state);
+
+  void update() {
+    state.render(() => state.editMode = false);
+  }
+
+  void edit() {
+    state.render(() => state.editMode = true);
+  }
 }
--- lib/screen/signup_screen.dart
+++ lib/screen/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lesson3/controller/firebasecontroller.dart';
+import 'package:lesson3/screen/myview/mydialog.dart';
 
 class SignUpScreen extends StatefulWidget {
   static const routeName = '/signUpScreen';
@@ -10,6 +12,7 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpState extends State<SignUpScreen> {
   _Controller con;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -25,7 +28,62 @@ class _SignUpState extends State<SignUpScreen> {
       appBar: AppBar(
         title: Text('Create an account'),
       ),
-      body: Text('sign up'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 15.0),
+        child: Form(
+            key: formKey,
+            child: SingleChildScrollView(
+                child: Column(
+              children: [
+                Text(
+                  'Create an account',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  validator: con.validateEmail,
+                  onSaved: con.saveEmail,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                  ),
+                  obscureText: true,
+                  autocorrect: false,
+                  validator: con.validatePassword,
+                  onSaved: con.savePassword,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Password confirm',
+                  ),
+                  obscureText: true,
+                  autocorrect: false,
+                  validator: con.validatePassword,
+                  onSaved: con.savePasswordConfirm,
+                ),
+                con.passwordErrorMessage == null
+                    ? SizedBox(
+                        height: 1.0,
+                      )
+                    : Text(
+                        con.passwordErrorMessage,
+                        style: TextStyle(color: Colors.red, fontSize: 14.0),
+                      ),
+                RaisedButton(
+                  onPressed: con.createAccount,
+                  child: Text(
+                    'Create',
+                    style: Theme.of(context).textTheme.button,
+                  ),
+                ),
+              ],
+            ))),
+      ),
     );
   }
 }
@@ -33,4 +91,57 @@ class _SignUpState extends State<SignUpScreen> {
 class _Controller {
   _SignUpState state;
   _Controller(this.state);
+  String email;
+  String password;
+  String passwordConfirm;
+  String passwordErrorMessage;
+
+  void createAccount() async {
+    if (!state.formKey.currentState.validate()) return;
+    state.render(() => passwordErrorMessage = null);
+    state.formKey.currentState.save();
+
+    if (password != passwordConfirm) {
+      state.render(() => passwordErrorMessage = 'Passwords do not match!');
+      return;
+    }
+
+    try {
+      await FirebaseController.createAccount(email: email, password: password);
+      MyDialog.info(
+        context: state.context,
+        title: 'Account Created!',
+        content: 'Go to Sign In to use the app',
+      );
+    } catch (e) {
+      MyDialog.info(
+          context: state.context, title: 'Cannot create', content: '$e');
+    }
+  }
+
+  String validateEmail(String value) {
+    if (value.contains('@') && value.contains('.'))
+      return null;
+    else
+      return 'invalid email';
+  }
+
+  void saveEmail(String value) {
+    email = value;
+  }
+
+  String validatePassword(String value) {
+    if (value.length < 6)
+      return 'too short';
+    else
+      return null;
+  }
+
+  void savePassword(String value) {
+    password = value;
+  }
+
+  void savePasswordConfirm(String value) {
+    passwordConfirm = value;
+  }
 }
